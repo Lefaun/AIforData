e01eb225105468bca2baa1f92ffa633341d50b72
Lefaun <118660975+Lefaun@users.noreply.github.com>
2024-01-19 15:18:24 +0000

    Update ApiKey.py

--- ApiKey.py
+++ ApiKey.py
@@ -1,2 +1,1 @@
-apikey = "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"
-#apikey = "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"
+Apikey = "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"
