08b1dd80d1880fdc616792965d9a29fd8ba5c995
Lefaun <118660975+Lefaun@users.noreply.github.com>
2024-01-20 11:36:22 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,8 +1,9 @@
 #Import required libraries
 import os
-#os.environ['OPENAI_KEY']
-#load_dotenv(find_dotenv())
+os.environ['OPENAI_KEY']
+
 #os.environ.get('OPENAI_KEY')
+load_dotenv(find_dotenv())
 #from Apikey import apikey 
 
 import streamlit as st
